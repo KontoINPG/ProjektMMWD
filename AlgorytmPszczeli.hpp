f6b04e40331149cbd000ad7e25826d3f57ea7fea
Wojciech Kula <w.kula14@gmail.com>
2018-12-19 12:06:43 +0100

    Added declaration of goal_fcn

--- AlgorytmPszczeli.hpp
+++ AlgorytmPszczeli.hpp
@@ -2,7 +2,8 @@
 #define ALGORYTM_PSZCZELI
 #include"produkt.hpp"
 
-void algorytm_pszczeli_testy();
 
+void algorytm_pszczeli_testy();
+int f_celu (std::vector <int> roz);
 
 #endif // ALGORYTM_PSZCZELI
