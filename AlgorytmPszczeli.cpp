539a578d990f7412d340d67e2610e01f3333c7bd
Inpg <kontoinpg@gmail.com>
2018-12-21 15:53:20 +0100

    Dodana funkcja: "losujRozwiazaniaZOtoczenia". Umożliwia generowanie rozwiązań początkowych i w dalszej części programu zadanej liczby rozwiązań o zadanej mierze Hamminga wokół podanego rozwiązania.

--- AlgorytmPszczeli.cpp
+++ AlgorytmPszczeli.cpp
@@ -11,40 +11,102 @@ using std::endl;
 
 // http://zsi.ii.us.edu.pl/~mboryczka/IntStad/sr_abc.php
 
+
+std::vector<std::vector<int>> losujRozwiazaniaZOtoczenia(std::vector<int> _rozwiazanieBazowe, int _iloscGenerowanychRozwiazan, int _miaraHamminga, std::vector<std::vector<int>> _macierzDostawcow)
+{
+    int dlugoscRozwiazania = _rozwiazanieBazowe.size();
+    std::vector<int> rozwiazanieWyjsciowe = _rozwiazanieBazowe;
+
+    std::vector<std::vector<int>> wektorWylosowanychRozwiazan;
+
+
+    for(int lRozwiazan=0;lRozwiazan<_iloscGenerowanychRozwiazan;lRozwiazan++)
+    {
+
+    for(int lHamming=0;lHamming<_miaraHamminga;lHamming++)
+    {
+
+        std::vector<int> dostawcy;
+        int ilDostawcow;
+
+        int wylosowanaPozycja;
+
+        if(_miaraHamminga==dlugoscRozwiazania)//Dzieki temu mozna losowac wszystkie elementy wektora na nowo, podajac miare Hamminga = dl. wektora;
+        {
+            wylosowanaPozycja = lHamming;
+        }else{
+                wylosowanaPozycja = rand()%dlugoscRozwiazania;
+                }
+
+        dostawcy = _macierzDostawcow[wylosowanaPozycja];
+        ilDostawcow = dostawcy.size();
+        rozwiazanieWyjsciowe[wylosowanaPozycja] = dostawcy[rand()%ilDostawcow];
+
+    }
+
+    wektorWylosowanychRozwiazan.push_back(rozwiazanieWyjsciowe);
+     //Wyswietlanie, dla testu;
+        for(lA:rozwiazanieWyjsciowe)
+            cout<<BazaSklepow[lA].get_nazwa()<<", ";
+        cout<<endl;
+        cout<<"------------------"<<endl;
+    }
+    return wektorWylosowanychRozwiazan;
+
+}
 void algorytm_pszczeli_testy()
 {
 
-    //Losowanie początkowych rozwi¹zañ:
+    //Losowanie poczatkowych rozwiazan:
 
-    int ilFurazerek = 5;
+    std::vector <std::vector<int>> macierzDostawcow;
+
+    for ( element:ListaZamowienia)
+    {
+        std::vector<int> dostawcy = element.get_listaDostawcow();
+        macierzDostawcow.push_back(dostawcy);
+    }
+
+
+    std::vector<int> rozwiazaniePomocniczeStartowe;//Tylko do wystartowania losujRozwiazaniaZOtoczenia potrzebne;
+
+    for(int lElem=0;lElem<ListaZamowienia.size();lElem++)
+        {
+            rozwiazaniePomocniczeStartowe.push_back(0);
+        }
+
+
+    losujRozwiazaniaZOtoczenia(rozwiazaniePomocniczeStartowe,5,ListaZamowienia.size(),macierzDostawcow);//losowanie z miarą Hamminga = il. zamawianych elem. => czyli losowanie wszystkich elementów wektora;
+
+    /*int ilFurazerek = 5;
     std::vector< std::vector<int>> rozwiazaniePoczatkowe;
 
     for (int furazerka = 0; furazerka<ilFurazerek; furazerka++)
     {
         std::vector<int> rozwiazanie;
-        for( element : ListaZamowienia)
+        int iloscElementowZamowienia = ListaZamowienia.size();
+
+        for(int lElem=0;lElem<iloscElementowZamowienia;lElem++)
         {
             int wylosowanySklepID;
             std::vector<int> dostawcy;
             int ilDostawcow;
 
-            dostawcy = element.get_listaDostawcow();
+            dostawcy = macierzDostawcow[lElem];
             ilDostawcow = dostawcy.size();
             wylosowanySklepID = dostawcy[rand()%ilDostawcow];
             rozwiazanie.push_back(wylosowanySklepID);
         }
         rozwiazaniePoczatkowe.push_back(rozwiazanie);
-
-        //Wyświetlanie, dla testu;
-        for(lA:rozwiazanie)
-            cout<<BazaSklepow[lA].get_nazwa()<<", ";
-        cout<<endl;
-        cout<<"------------------"<<endl;
-    }
+*/
 
 
-	while (1)
-	{
+
+   // }
+
+
+	//while (1)
+	//{
 		//sprawdzenie rozwiazan
 
 		//wybranie elity i najlepszych. Zapamietanie najlepszego rozwiazania.
@@ -58,7 +120,7 @@ void algorytm_pszczeli_testy()
 		//generowanie kolejnej populcji
 
 
-	}
+	//}
 }//void algorytm_pszczeli_testy();
 
 
--- AlgorytmPszczeli.hpp
+++ AlgorytmPszczeli.hpp
@@ -8,5 +8,6 @@
 
 void algorytm_pszczeli_testy();
 int oblicz_funkcje_celu(std::vector<int> _wektorRozwiazan);
+std::vector<std::vector<int>> losujRozwiazaniaZOtoczenia(std::vector<int> _rozwiazanieBazowe, int _iloscGenerowanychRozwiazan, int _miaraHamminga, std::vector<std::vector<int>> _macierzDostawcow);
 
 #endif // ALGORYTM_PSZCZELI
