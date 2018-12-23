38332dc7023800dfc8e4c9edb90dc9d4ed882308
Wojciech Kula <w.kula14@gmail.com>
2018-12-23 07:31:14 +0100

    Changed declaration of fcn; Added file name parameter

--- pliki.hpp
+++ pliki.hpp
@@ -1,11 +1,11 @@
 #ifndef PLIKI_HPP
 #define PLIKI_HPP
 #include<vector>
-
+#include<string>
 
 int wczytajBaze();
 
 int wczytajZamowienie();
 
-int zapiszZamowienie(std::vector <int> Wynik);
+int zapiszZamowienie(std::vector <int> Wynik, std::string plik_nazwa);
 #endif // PLIKI_HPP
