59e6fd32969e26d490f665934ad88af2d3077475
Wojciech Kula <w.kula14@gmail.com>
2019-01-13 16:28:42 +0100

    Removed useless variable

--- Parametry.cpp
+++ Parametry.cpp
@@ -158,18 +158,6 @@ void ParametryProgramu::set_wspKaraJakosc(double _wspKaraJakosc)
 
 //-------------------//
 
-int ParametryProgramu::get_ilosc_roz()
-{
-    return ilosc_roz;
-}
-
-void ParametryProgramu::set_ilosc_roz(int _ilosc_roz)
-{
-    ilosc_roz = _ilosc_roz;
-}
-
-//-------------------//
-
 bool ParametryProgramu::get_rodziceWPopulacji()
 {
     return rodziceWPopulacji;
